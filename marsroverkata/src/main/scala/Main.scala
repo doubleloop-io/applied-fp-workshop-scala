3cb6090a2e57c36f15b89d19217065a1139b73ae
Matteo Baglini <matteo.baglini@gmail.com>
2019-06-10 15:15:01 +0200

    compute and apply delta to move

--- marsroverkata/src/main/scala/Main.scala
+++ marsroverkata/src/main/scala/Main.scala
@@ -1,3 +1,8 @@
 package marsroverkata
 
-object Main extends App {}
+object Main extends App {
+
+//   marsroverkata.answers.Version1.example1()
+//   marsroverkata.answers.Version1.example2()
+
+}
--- marsroverkata/src/main/scala/answers/Game1.scala
+++ marsroverkata/src/main/scala/answers/Game1.scala
@@ -1,127 +1,133 @@
 package marsroverkata.answers
 
-import scala.io.StdIn._
-
-import cats.effect.IO
-
 object Version1 {
-  class Game {
 
-    def example1(): Unit = {
-      val mission  = Mission(Planet(Size(5, 4)), Rover(Position(0, 0), N))
-      val commands = List(Turn(OnRight), Move(Forward))
-      val updated  = execute(mission, commands)
-      println(updated)
-      assert(updated == Mission(Planet(Size(5, 4)), Rover(Position(0, 1), E)))
-    }
+//   def example1(): Unit = {
+//     val mission = Mission(Planet(Size(5, 4)), Rover(Position(0, 0), N))
+//     println(
+//       "last >> " +
+//         execute(mission,
+//                 List(
+//                   Turn(OnLeft),
+//                   Turn(OnLeft),
+//                   Turn(OnLeft),
+//                   Move(Forward),
+//                   Move(Forward),
+//                   Move(Forward),
+//                   Move(Forward),
+//                   Move(Forward),
+//                   Move(Forward),
+//                 ))
+//     )
+//   }
 
-    def example2(): Unit = {
-      val mission  = Mission(Planet(Size(5, 4)), Rover(Position(0, 0), N))
-      val commands = List(Move(Forward), Move(Forward), Turn(OnLeft), Move(Backward))
-      val updated  = execute(mission, commands)
-      println(updated)
-      assert(updated == Mission(Planet(Size(5, 4)), Rover(Position(3, 1), W)))
-    }
-
-    def execute(mission: Mission, commands: List[Command]): Mission =
-      commands.foldLeft(mission)(execute)
-
-    def execute(mission: Mission, command: Command): Mission =
-      mission.copy(rover = command match {
-        case Turn(tt) => turn(mission.rover, tt)
-        case Move(mt) => move(mission.rover, mission.planet, mt)
-      })
-
-    def turn(rover: Rover, turn: TurnType): Rover =
-      rover.copy(direction = turn match {
-        case OnRight => turnRight(rover.direction)
-        case OnLeft  => turnLeft(rover.direction)
-      })
-
-    def turnRight(direction: Direction): Direction =
-      direction match {
-        case N => E
-        case E => S
-        case S => W
-        case W => N
-      }
-
-    def turnLeft(direction: Direction): Direction =
-      direction match {
-        case N => W
-        case W => S
-        case S => E
-        case E => N
-      }
-
-    def move(rover: Rover, planet: Planet, move: MoveType): Rover =
-      rover.copy(position = move match {
-        case Forward  => forward(rover, planet)
-        case Backward => backward(rover, planet)
-      })
-
-    def forward(rover: Rover, planet: Planet): Position =
-      next(rover.position, rover.direction, planet)
-
-    def backward(rover: Rover, planet: Planet): Position =
-      next(rover.position, opposite(rover.direction), planet)
-
-    def opposite(direction: Direction): Direction =
-      direction match {
-        case N => S
-        case S => N
-        case E => W
-        case W => E
-      }
-
-    def next(position: Position, direction: Direction, planet: Planet): Position =
-      direction match {
-        case N => nextUp(position, planet)
-        case E => nextRight(position, planet)
-        case W => nextLeft(position, planet)
-        case S => nextDown(position, planet)
-      }
-
-    def nextUp(position: Position, planet: Planet): Position =
-      setX(position, (position.x - 1 + planet.size.x) % planet.size.x)
-
-    def nextDown(position: Position, planet: Planet): Position =
-      setX(position, (position.x + 1) % planet.size.x)
-
-    def nextLeft(position: Position, planet: Planet): Position =
-      setY(position, (position.y - 1 + planet.size.y) % planet.size.y)
-
-    def nextRight(position: Position, planet: Planet): Position =
-      setY(position, (position.y + 1) % planet.size.y)
-
-    def setX(position: Position, value: Int): Position =
-      position.copy(x = value)
-
-    def setY(position: Position, value: Int): Position =
-      position.copy(y = value)
-
-    case class Position(x: Int, y: Int)
-    case class Size(x: Int, y: Int)
-    case class Planet(size: Size)
-    case class Rover(position: Position, direction: Direction)
-    case class Mission(planet: Planet, rover: Rover)
-
-    sealed trait Command
-    case class Move(direction: MoveType) extends Command
-    case class Turn(direction: TurnType) extends Command
-
-    sealed trait MoveType
-    case object Forward  extends MoveType
-    case object Backward extends MoveType
-
-    sealed trait TurnType
-    case object OnRight extends TurnType
-    case object OnLeft  extends TurnType
-
-    sealed trait Direction
-    case object N extends Direction
-    case object E extends Direction
-    case object W extends Direction
-    case object S extends Direction
+  def example1(): Unit = {
+    val mission  = Mission(Planet(Size(5, 4)), Rover(Position(0, 0), N))
+    val commands = List(Turn(OnRight), Move(Forward))
+    val updated  = execute(mission, commands)
+    println(updated)
+    assert(updated == Mission(Planet(Size(5, 4)), Rover(Position(1, 0), E)))
   }
+
+  def example2(): Unit = {
+    val mission  = Mission(Planet(Size(5, 4)), Rover(Position(0, 1), E))
+    val commands = List(Turn(OnRight), Move(Forward))
+    val commands = List(Move(Forward), Move(Forward), Turn(OnLeft), Move(Backward))
+    val updated = execute(mission, commands)
+    println(updated)
+    assert(updated == Mission(Planet(Size(5, 4)), Rover(Position(3, 1), S)))
+  }
+
+  def execute(mission: Mission, commands: List[Command]): Mission =
+    commands.foldLeft(mission)(execute)
+
+  def execute(mission: Mission, command: Command): Mission =
+    mission.copy(rover = command match {
+      case Turn(tt) => turn(mission.rover, tt)
+      case Move(mt) => move(mission.rover, mission.planet, mt)
+    })
+
+  def turn(rover: Rover, turn: TurnType): Rover =
+    rover.copy(direction = turn match {
+      case OnRight => turnRight(rover.direction)
+      case OnLeft  => turnLeft(rover.direction)
+    })
+
+  def turnRight(direction: Direction): Direction =
+    direction match {
+      case N => E
+      case E => S
+      case S => W
+      case W => N
+    }
+
+  def turnLeft(direction: Direction): Direction =
+    direction match {
+      case N => W
+      case W => S
+      case S => E
+      case E => N
+    }
+
+  def move(rover: Rover, planet: Planet, move: MoveType): Rover =
+    rover.copy(position = move match {
+      case Forward  => forward(rover, planet)
+      case Backward => backward(rover, planet)
+    })
+
+  def forward(rover: Rover, planet: Planet): Position =
+    next(rover.position, planet.size, delta(rover.direction))
+
+  def backward(rover: Rover, planet: Planet): Position =
+    next(rover.position, planet.size, delta(opposite(rover.direction)))
+
+  def opposite(direction: Direction): Direction =
+    direction match {
+      case N => S
+      case S => N
+      case E => W
+      case W => E
+    }
+
+  def delta(direction: Direction): Delta =
+    direction match {
+      case N => Delta(0, 1)
+      case S => Delta(0, -1)
+      case E => Delta(1, 0)
+      case W => Delta(-1, 0)
+    }
+
+  def wrap(axis: Int, size: Int, delta: Int): Int =
+    (((axis + delta) % size) + size) % size
+
+  def next(position: Position, size: Size, delta: Delta): Position =
+    position.copy(
+      x = wrap(position.x, size.x, delta.x),
+      y = wrap(position.y, size.y, delta.y)
+    )
+
+  case class Delta(x: Int, y: Int)
+  case class Position(x: Int, y: Int)
+  case class Size(x: Int, y: Int)
+  case class Planet(size: Size)
+  case class Rover(position: Position, direction: Direction)
+  case class Mission(planet: Planet, rover: Rover)
+
+  sealed trait Command
+  case class Move(direction: MoveType) extends Command
+  case class Turn(direction: TurnType) extends Command
+
+  sealed trait MoveType
+  case object Forward  extends MoveType
+  case object Backward extends MoveType
+
+  sealed trait TurnType
+  case object OnRight extends TurnType
+  case object OnLeft  extends TurnType
+
+  sealed trait Direction
+  case object N extends Direction
+  case object E extends Direction
+  case object W extends Direction
+  case object S extends Direction
 }
