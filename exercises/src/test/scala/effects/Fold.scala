83e75384344f387dda459471810a8cf9286c0fee
Matteo Baglini <matteo.baglini@gmail.com>
2019-06-11 11:59:30 +0200

    effects: add fold to remove it

---
+++ exercises/src/test/scala/effects/Fold.scala
@@ -0,0 +1,26 @@
+package exercises
+
+import minitest._
+
+object FoldTests extends SimpleTestSuite {
+
+  case class Item(qty: Int)
+
+  def createItem(qty: String): Option[Item] =
+    if (qty.matches("^[0-9]+$")) Some(Item(qty.toInt))
+    else None
+
+  test("valid creation") {
+    val item = createItem("100")
+    ignore("use fold to make test green")
+    val result = ???
+    assertEquals(result, "100")
+  }
+
+  test("invalid creation") {
+    val item = createItem("asd")
+    ignore("use fold to make test green")
+    val result = ???
+    assertEquals(result, "alternative value")
+  }
+}
---
+++ exercises/src/test/scala/effects/answers/Fold.scala
@@ -0,0 +1,24 @@
+package exercises.answers
+
+import minitest._
+
+object FoldTests extends SimpleTestSuite {
+
+  case class Item(qty: Int)
+
+  def createItem(qty: String): Option[Item] =
+    if (qty.matches("^[0-9]+$")) Some(Item(qty.toInt))
+    else None
+
+  test("valid creation") {
+    val item   = createItem("100")
+    val result = item.fold("alternative value")(_.qty.toString)
+    assertEquals(result, "100")
+  }
+
+  test("invalid creation") {
+    val item   = createItem("asd")
+    val result = item.fold("alternative value")(_.qty.toString)
+    assertEquals(result, "alternative value")
+  }
+}
