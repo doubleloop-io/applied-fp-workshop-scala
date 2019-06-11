79372ba1421c46835cc0836c49c26a6270e5e94b
Matteo Baglini <matteo.baglini@gmail.com>
2019-06-11 12:12:36 +0200

    effects: add traverse

---
+++ exercises/src/test/scala/effects/Traverse.scala
@@ -0,0 +1,38 @@
+package exercises
+
+import minitest._
+
+object TraverseTests extends SimpleTestSuite {
+
+  import cats.implicits._
+
+  case class Item(qty: Int)
+
+  def createItem(qty: String): Option[Item] =
+    if (qty.matches("^[0-9]+$")) Some(Item(qty.toInt))
+    else None
+
+  test("all valid creation - idividual result with map") {
+    val values = List("1", "10", "100")
+    val items  = values.map(createItem)
+    ignore("write the assert")
+  }
+
+  test("some invalid creation - idividual result with map") {
+    val values = List("1", "asf", "100")
+    val items  = values.map(createItem)
+    ignore("write the assert")
+  }
+
+  test("all valid creation - omni result with traverse") {
+    val values = List("1", "10", "100")
+    val items  = values.traverse(createItem)
+    ignore("write the assert")
+  }
+
+  test("some invalid creation - omni result with traverse") {
+    val values = List("1", "asd", "100")
+    val items  = values.traverse(createItem)
+    ignore("write the assert")
+  }
+}
---
+++ exercises/src/test/scala/effects/answers/Traverse.scala
@@ -0,0 +1,38 @@
+package exercises.answers
+
+import minitest._
+
+object TraverseTests extends SimpleTestSuite {
+
+  import cats.implicits._
+
+  case class Item(qty: Int)
+
+  def createItem(qty: String): Option[Item] =
+    if (qty.matches("^[0-9]+$")) Some(Item(qty.toInt))
+    else None
+
+  test("all valid creation - idividual result with map") {
+    val values = List("1", "10", "100")
+    val items  = values.map(createItem)
+    assertEquals(items, List(Some(Item(1)), Some(Item(10)), Some(Item(100))))
+  }
+
+  test("some invalid creation - idividual result with map") {
+    val values = List("1", "asf", "100")
+    val items  = values.map(createItem)
+    assertEquals(items, List(Some(Item(1)), None, Some(Item(100))))
+  }
+
+  test("all valid creation - omni result with traverse") {
+    val values = List("1", "10", "100")
+    val items  = values.traverse(createItem)
+    assertEquals(items, Some(List(Item(1), Item(10), Item(100))))
+  }
+
+  test("some invalid creation - omni result with traverse") {
+    val values = List("1", "asd", "100")
+    val items  = values.traverse(createItem)
+    assertEquals(items, None)
+  }
+}
