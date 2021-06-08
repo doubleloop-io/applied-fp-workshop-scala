2950b9cb6ff42fcc385c196e93db018c9b0963a8
Matteo Baglini <matteo.baglini@gmail.com>
2021-06-08 23:10:17 +0200

    better test names

--- exercises/src/test/scala/effects/answers/Lazy.scala
+++ exercises/src/test/scala/effects/answers/Lazy.scala
@@ -2,12 +2,12 @@ package effects.answers
 
 class LazyTests extends munit.FunSuite {
 
-  case class Lazy[A](value: () => A) {
+  case class Lazy[A](run: () => A) {
     def map[B](f: A => B): Lazy[B] =
-      Lazy.pure(() => f(value()))
+      Lazy.pure(() => f(run()))
 
     def flatMap[B](f: A => Lazy[B]): Lazy[B] =
-      Lazy.pure(() => f(value()).value())
+      Lazy.pure(() => f(run()).run())
   }
 
   object Lazy {
@@ -23,26 +23,26 @@ class LazyTests extends munit.FunSuite {
   def reversedString(x: Int): Lazy[String] =
     Lazy.pure(() => x.toString.reverse)
 
-  test("lift a value into a container") {
+  test("lift a value and run the effect") {
     val c = Lazy
       .pure(expensiveComputation _)
 
-    assertEquals(c.value(), 10)
+    assertEquals(c.run(), 10)
   }
 
-  test("chain not container-aware functions") {
+  test("chain pure function") {
     val c = Lazy
       .pure(expensiveComputation _)
       .map(increment)
 
-    assertEquals(c.value(), 11)
+    assertEquals(c.run(), 11)
   }
 
-  test("chain container-aware functions") {
+  test("chain effectful function") {
     val c = Lazy
       .pure(expensiveComputation _)
       .flatMap(reversedString)
 
-    assertEquals(c.value(), "01")
+    assertEquals(c.run(), "01")
   }
 }
