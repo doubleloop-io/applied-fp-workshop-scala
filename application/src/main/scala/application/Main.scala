49af79d721c7fae14de9cad023b367c83c7b76a9
Matteo Baglini <matteo.baglini@gmail.com>
2022-09-13 15:21:58 +0200

    prepare application Main code

--- application/src/main/scala/application/Main.scala
+++ application/src/main/scala/application/Main.scala
@@ -1,3 +1,13 @@
 package application
 
-object Main extends App
+object Main extends App {
+
+  // TODO: uncomment the following lines after completing exercise Version5
+
+  // import application.Version5._
+  // import cats.effect.unsafe.implicits.global
+
+  // val app = createApplication("planet.txt", "rover.txt")
+  // app.unsafeRunSync()
+
+}
